2135f620a6bbb186cad3ff42c45fa510f394cc4a
Abdulrahman Hisham <cole13517@gmail.com>
2020-09-04 01:54:34 +0400

    added a feature to select a file to encrypt

--- simpleGUI.py
+++ simpleGUI.py
@@ -2,8 +2,11 @@ import PySimpleGUI as sg
 import digitalcypher
 sg.theme('Purple')
 layout = [  [sg.Text('Please enter the string to be encoded/decoded and the key: ')],
-            [sg.InputText(k='-IN-')],
-            [sg.InputText(k='-IN2-')],
+            #[sg.InputText(k='-IN-')],
+            #[sg.InputText(k='-IN2-')],
+            [sg.Text('String', size=(8, 1)), sg.Input(k='-IN-'), sg.FileBrowse()],
+            [sg.Text('Key',size=(8, 1)), sg.Input(k='-IN2-')],
+            #[sg.Text('File 1', size=(8, 1)), sg.Input(), sg.FileBrowse()],
             [sg.Button('Encode'), sg.Button('Decode'), sg.Button('Cancel')],
             [sg.Text('Converted DMS Coordinates', key='-OUTPUT-')],
             [sg.Input(key='-OUT')] 
@@ -16,10 +19,28 @@ while True:
     if event == sg.WIN_CLOSED or event == 'Cancel':	
         break
     if event == 'Encode':
-        convertedVal = digitalcypher.encode(values['-IN-'],values['-IN2-'])
-        print('You entered ', convertedVal)
-        #window['-OUTPUT-'].update(convertedVal)
-        window['-OUT'].update(convertedVal)
+        # add a conditional that checks the first letter of the string 
+        # if the first letter of the string is / (basically a path)
+        # then we'd access the file itself 
+        if values['-IN-'][0] == '/':
+            f = open(values['-IN-'], 'r')
+            writefile = open("/home/abdulrahman/Desktop/test.txt",'w')
+            fread = f.read()
+            f.close()
+            print(fread)
+            convertedVal = digitalcypher.encode(fread,values['-IN2-'])
+            print('You entered ', convertedVal)
+            writefile.write(convertedVal)
+            writefile.close()
+            #window['-OUT'].update(convertedVal)
+        #fix crash by writing the output of the if statement to a file instead of updating the window with it 
+        #fixed 
+        else:
+            print('Original Value', values['-IN-'])
+            convertedVal = digitalcypher.encode(values['-IN-'],values['-IN2-'])
+            print('You entered ', convertedVal)
+            #window['-OUTPUT-'].update(convertedVal)
+            window['-OUT'].update(convertedVal)
     if event == 'Decode':
         convertedVal = digitalcypher.decode(values['-IN-'],values['-IN2-'])
         print('You entered ', convertedVal)
